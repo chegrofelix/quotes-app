403693f1918e0f6317b1c7b61335d050a1a9f8dd
chegrofelix <chegerofelix@gmail.com>
2021-02-01 10:41:59 +0300

    Added class quotes

--- quotes-app/src/app/quotes-form/quotes-form.component.ts
+++ quotes-app/src/app/quotes-form/quotes-form.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesFormComponent implements OnInit {
 
-  constructor() { }
+
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
   }

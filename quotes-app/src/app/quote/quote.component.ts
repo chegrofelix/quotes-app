4faa68fde6fc27d5de6d8df1c5730052f1204483
chegrofelix <chegerofelix@gmail.com>
2021-02-02 00:25:50 +0300

    modified the quote component.ts

--- quotes-app/src/app/quote/quote.component.ts
+++ quotes-app/src/app/quote/quote.component.ts
@@ -9,11 +9,11 @@ import { Quote } from '../quote'
 export class QuoteComponent implements OnInit {
 
   userQuotes = [
-    new Quote(1, 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', '-	Martin Fowler', 0, 0, new Date(2021,1,5), 'sam'),
-    new Quote(2, 'First, solve the problem. Then, write the code.', '-	John Johnson', 0, 0, new Date(2021,7,10), 'david'),
-    new Quote(3, 'AIn order to be irreplaceable, one must always be different.', 'Coco Chanel', 0, 0, new Date(2021,1,14), 'kanyutu'),
-    new Quote(4, 'Java is to JavaScript what car is to Carpet.',  'Chris Heilmann', 0, 0, new Date(2021,1,10), 'Weshy'),
-    new Quote(5, 'Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code.', '-	Dan Salomon', 0, 0, new Date(2021,1,28), 'rose')
+    new Quote(1, ' The only place where success comes before work is in the dictionary.', '-	Vidal Sassoon', 0, 0, new Date(2021,1,27), 'Rix'),
+    new Quote(2, 'The pessimist sees difficulty in every opportunity.The optimist sees opportunity in every difficulty.', '-	Winston Churchill', 0, 0, new Date(2021,2,1), 'Celine'),
+    new Quote(3, 'Do not let yesterday take up too much of today.', 'Will Rogers', 0, 0, new Date(2021,3,5), 'Esther'),
+    new Quote(4, 'Simplicity is the soul of efficiency.',  'Austin Freeman', 0, 0, new Date(2021,1,1), 'David'),
+    new Quote(5, 'The best thing about the future is that it comes one day at a time.', '-	Abraham Licoln', 0, 0, new Date(2021,1,2), 'Felix')
   ]
 
   toogleDetails(index:any){

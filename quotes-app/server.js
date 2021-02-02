5cb1a5327da1a7f62eac5feda2aa376951a07ace
chegrofelix <chegerofelix@gmail.com>
2021-02-02 09:39:26 +0300

    modified sever.js

--- quotes-app/server.js
+++ quotes-app/server.js
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.static(__dirname + "/dist/quotes-app"));
 
 app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(__dirname + "/dist/quotes-app/index.html"));
 });
 app.listen(process.env.PORT||8080);
